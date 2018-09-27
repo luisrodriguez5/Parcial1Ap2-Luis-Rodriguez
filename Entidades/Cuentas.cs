d17ae599e79c74befa37d554b4f71bc40ec04bbf
Luis German Rodriguez Reynoso <luis_rodriguez5@ucne.edu.do>
2018-09-27 10:44:35 -0400

    Agregando DataBase

---
+++ Entidades/Cuentas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cuentas
+    {
+        public int CuentaId { get; set; }
+        public DateTime Fecha  { get; set; }
+        public string Nombre { get; set; }
+        public decimal Balance { get; set; }
+
+        public Cuentas()
+        {
+
+        }
+        public Cuentas(int cuentaId, DateTime fecha, string nombre, decimal balance )
+        {
+            this.CuentaId = cuentaId;
+            this.Fecha = fecha;
+            this.Nombre = nombre;
+            this.Balance = balance;
+
+
+        }
+    }
+}
