1f35d1f07b79fc72384caef003ec6ca8ff09b81d
Luis German Rodriguez Reynoso <luis_rodriguez5@ucne.edu.do>
2018-09-27 11:15:36 -0400

    Preparando BLL

---
+++ DAL/ParcialDb1.cs
@@ -0,0 +1,20 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ParcialDb1 : DbContext
+    {
+        public ParcialDb1() : base("ConStr") 
+        {
+
+        }
+        public DbSet<Cuentas> Cuentas { get; set; }
+
+    }
+}
